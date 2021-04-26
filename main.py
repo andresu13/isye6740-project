483cebcfb75c25563d1ce7ec391cdb9af4cbb7bb
Andres Urrutia <andresu13@gmail.com>
2021-04-26 14:15:57 -0400

    removed temporary python files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logistic_model
-#import neural_model
+import neural_model
 #import svm_model
 import pandas as pd
 import numpy as np
@@ -7,17 +7,20 @@ from sklearn.model_selection import train_test_split
 
 
 def main():
-    np.random.seed(30)
+    #np.random.seed(30)
     stock_df = pd.read_csv("sample_stock_data.csv")
-    X_train, X_test, y_train, y_test = train_test_split(stock_df.iloc[:,0:-1],stock_df.iloc[:,-1], test_size=0.8)
+    X_train, X_test, y_train, y_test = train_test_split(stock_df.iloc[:,0:-1],stock_df.iloc[:,-1], test_size=0.2)
     #print(X_train)
-    print(X_train.head())
+    #X_train.to_csv("sample_stock_data_train.csv")
+    #y_train.to_csv("sample_stock_label_train.csv")
+    #X_test.to_csv("sample_stock_data_test.csv")
+    #y_test.to_csv("sample_stock_label_test.csv")
 
     ### These 3 lines will be called once the models are built
     ### Expected API Return: Accuracy, Recall, F1 Score
 
     #log_ml = logistic_model.build_model(X_train, X_test, y_train, y_test )
-    #neural_ml = neural_model.build_model(X_train, X_test, y_train, y_test )
+    neural_ml = neural_model.build_model(X_train, X_test, y_train, y_test )
     #svm_ml = svm_model.build_model(X_train, X_test, y_train, y_test )
 
 
