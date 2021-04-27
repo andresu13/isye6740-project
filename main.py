7759ae25d7a381bb07a0b8128102c0cf9f8c7ee2
Andres Urrutia <andresu13@gmail.com>
2021-04-27 19:57:53 -0400

    updated main with bagging accuracy

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import neural_model as nm
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 def main():
@@ -13,22 +14,21 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(stock_df.iloc[:,0:-1],stock_df.iloc[:,-1], test_size=0.2)
     #print(X_train)
 
-    
-    #X_train.to_csv("sample_stock_data_train.csv")
-    #y_train.to_csv("sample_stock_label_train.csv")
-    #X_test.to_csv("sample_stock_data_test.csv")
-    #y_test.to_csv("sample_stock_label_test.csv")
-
     ### These 3 lines will be called once the models are built
     ### Expected API Return: Accuracy, Recall, F1 Score
 
     ### Build models
     #log_ml = logistic_model.build_model(X_train, X_test, y_train, y_test )
+
+    ### BUILD EACH MODEL AND GET PREDICTIONS FOR EACH MODEL
     neural_ml = nm.NeuralModel(X_train, X_test, y_train, y_test)
     neural_ml.build_model()
-    print(neural_ml.predict(X_test))
-    #neural_ml = neural_model.build_model(X_train, X_test, y_train, y_test)
-    #svm_ml = svm_model.build_model(X_train, X_test, y_train, y_test )
+    neural_y_predict = neural_ml.predict(X_test)
+
+
+    ### CALCULATE FINAL ACCURACY BASED ON PREDICTIONS FROM ALL MODELS
+    print("BAGGING MODEL ACCURACY", accuracy_score(y_test, neural_y_predict))
+
 
 
     ### Fucntion that compares models
